f7ccb2ae9f1b8e4a216dcfdcd166a8faab3b072d
Dhruv Gupta <epost.dhruv@gmail.com>
2025-01-07 20:31:57 +0530

    Fix dependencies and remove shutil

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-import shutil
 from langchain_groq import ChatGroq
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
